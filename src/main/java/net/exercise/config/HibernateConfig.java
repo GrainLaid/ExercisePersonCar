d70ff613d04e7c0100e0ac7517d9588815343f7f
Alexandr Ivanov <alex13777@gmail.com>
2019-11-15 14:33:33 +0600

    Второй кумит

--- src/main/java/net/exercise/config/HibernateConfig.java
+++ src/main/java/net/exercise/config/HibernateConfig.java
@@ -1,6 +1,7 @@
 package net.exercise.config;
 import java.util.Properties;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,6 +13,8 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+import javax.sql.DataSource;
 
 import static org.hibernate.cfg.Environment.*;
 
@@ -28,29 +31,39 @@ public class HibernateConfig {
     public void setEnvironment(Environment environment) {
         this.environment = environment;
     }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.connection.release_mode", environment.getRequiredProperty("hibernate.connection.release_mode"));
+        properties.put("current_session_context_class", environment.getRequiredProperty("current_session_context_class"));
+        properties.put("hibernate.connection.autoReconnect", environment.getRequiredProperty("hibernate.connection.autoReconnect"));
+        properties.put("hbm2ddl.auto", environment.getRequiredProperty("hbm2ddl.auto"));
+        return properties;
+    }
+
     @Bean
-    public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-        Properties props = new Properties();
-        props.put(DRIVER, environment.getRequiredProperty("jdbc.driverClassName"));
-        props.put(URL, environment.getRequiredProperty("jdbc.url"));
-        props.put(USER, environment.getRequiredProperty("jdbc.username"));
-        props.put(PASS, environment.getRequiredProperty("jdbc.password"));
-        //Hiber
-        props.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        props.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        props.put("hibernate.connection.release_mode", environment.getRequiredProperty("hibernate.connection.release_mode"));
-        props.put("current_session_context_class", environment.getRequiredProperty("current_session_context_class"));
-        props.put("hibernate.connection.autoReconnect", environment.getRequiredProperty("hibernate.connection.autoReconnect"));
-        props.put("hbm2ddl.auto", environment.getRequiredProperty("hbm2ddl.auto"));
-        factoryBean.setHibernateProperties(props);
-        factoryBean.setPackagesToScan("net.exercise.entity");
-        return factoryBean;
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        return dataSource;
     }
     @Bean
-    public HibernateTransactionManager getTransactionManager() {
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("net.exercise.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+    @Bean
+    public HibernateTransactionManager transactionManager() {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        transactionManager.setSessionFactory(sessionFactory().getObject());
         return transactionManager;
     }
 }
